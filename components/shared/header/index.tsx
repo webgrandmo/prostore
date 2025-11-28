25e5f4cc715735ddb66f7b71cf207130620f2e3c
Andriy Trofimov <andriy.trofimov@valtech.com>
2025-11-28 19:33:35 +0200

    Sheet menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,8 @@
-import { ShoppingCart, UserIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { APP_NAME } from '@/lib/constants';
-import { Button } from '@/components/ui/button';
-import ModeToggle from './mode-toggle';
+
+import Menu from './menu';
 
 const Header = () => {
 	return (
@@ -23,24 +22,7 @@ const Header = () => {
 						<span className='hidden lg:block font-bold text-2xl ml-3'>{APP_NAME}</span>
 					</Link>
 				</div>
-				<div className='space-x-2'>
-					<ModeToggle />
-					<Button
-						asChild
-						variant={'ghost'}>
-						<Link href={'/cart'}>
-							<ShoppingCart />
-							Cart
-						</Link>
-					</Button>
-					<Button
-						asChild
-						variant={'default'}>
-						<Link href={'/sign-in'}>
-							<UserIcon /> Sign In
-						</Link>
-					</Button>
-				</div>
+				<Menu />
 			</div>
 		</header>
 	);
