624a9365a880afd7d429feb6e69f638e5c9662c5
Andriy Trofimov <andriy.trofimov@valtech.com>
2025-11-29 17:00:34 +0200

    Product list and product card with sample data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
 export const metadata = {
 	title: 'Home',
 };
 
 const Homepage = () => {
-	return <>Prostore</>;
+	return (
+		<>
+			<ProductList
+				data={sampleData.products}
+				title='Newest Arrivals'
+				limit={4}
+			/>
+		</>
+	);
 };
 
 export default Homepage;
---
+++ components/shared/product/product-card.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+
+const ProductCard = ({ product }: { product: any }) => {
+	return (
+		<Card className='w-full max-w-sm border-gray-200'>
+			<CardHeader className='p-0 items-center'>
+				<Link href={`/product/${product.slug}`}>
+					<Image
+						src={product.images[0]}
+						alt={product.name}
+						width={300}
+						height={300}
+						loading='eager'
+						priority={true}
+					/>
+				</Link>
+			</CardHeader>
+			<CardContent className='p-4 grid gap-4'>
+				<div className='text-xs'>{product.brand}</div>
+				<Link href={`/product/${product.slug}`}>
+					<h3 className='text-sm font-medium'>{product.name}</h3>
+				</Link>
+				<div className='flex-between gap-4'>
+					<p>{product.rating} Stars</p>
+					{product.stock > 0 ? (
+						<p className='font-bold'>{product.price}</p>
+					) : (
+						<p className='text-destructive'>Out Of Stock</p>
+					)}
+				</div>
+			</CardContent>
+		</Card>
+	);
+};
+
+export default ProductCard;
