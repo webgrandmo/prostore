cea7c764e926e15c24367521d955f4a3e3de6ca6
Andriy Trofimov <webgrandmo@gmail.com>
2026-01-01 17:29:38 +0200

    signUp schema and action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,11 @@
 'use server';
 
-import { signInFormSchema } from '../validators';
+import { signInFormSchema, signUpFormSchema } from '../validators';
 import { signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
+import { prisma } from '@/db/prisma';
+import { hashSync } from 'bcrypt-ts-edge';
+import { success } from 'zod';
 
 // Sign in the user with credentials
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
@@ -18,6 +21,41 @@ export async function signInWithCredentials(prevState: unknown, formData: FormDa
 			throw error;
 		}
 		return { success: false, message: 'Invalid email or password' };
+	}
+}
+
+// Sign up user with credentials
+export async function signUpWithCredentials(prevStatue: unknown, formData: FormData) {
+	try {
+		const user = signUpFormSchema.parse({
+			name: formData.get('name') as string,
+			email: formData.get('email') as string,
+			password: formData.get('password') as string,
+			confirmPassword: formData.get('confirmPassword') as string,
+		});
+
+		const plainPassword = user.password;
+
+		user.password = hashSync(user.password, 10);
+
+		await prisma.user.create({
+			data: {
+				name: user.name,
+				email: user.email,
+				password: user.password,
+			},
+		});
+
+		await signIn('credentials', {
+			email: user.email,
+			password: plainPassword,
+		});
+		return { success: true, message: 'User registered  successfully' };
+	} catch (error) {
+		if (isRedirectError(error)) {
+			throw error;
+		}
+		return { success: false, message: 'User was not registered' };
 	}
 }
 
--- lib/validators.ts
+++ lib/validators.ts
@@ -28,3 +28,16 @@ export const signInFormSchema = z.object({
 	email: z.email('Invalid email address'),
 	password: z.string().min(6, 'Password must be at least 6 characters long'),
 });
+
+// Schema for Sign Up form
+export const signUpFormSchema = z
+	.object({
+		name: z.string().min(3, 'Name must be at least 3 characters'),
+		email: z.email('Invalid email address'),
+		password: z.string().min(6, 'Password must be at least 6 characters long'),
+		confirmPassword: z.string().min(6, 'Confirm password must be at least 6 characters long'),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "Passwords don't match",
+		path: ['confirmPassword'],
+	});
